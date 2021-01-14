2bf300e2e941fe448b90d921a6fb7403e2b2e02b
JinwonPark <58999707+Dnopark@users.noreply.github.com>
2021-01-14 10:04:31 +0900

    day3 challenge

--- momontom/index.js
+++ momontom/index.js
@@ -1,7 +1,28 @@
-const title = document.querySelector("#title");
+import "./styles.css";
+const colors = ["#1abc9c", "#3498db", "#9b59b6", "#f39c12", "#e74c3c"];
 
-function handleClick() {
-    title.style.color = "red";
-}
+const title = document.querySelector("h1");
 
-title.addEventListener("click", handleClick);
+const superEventHandler = {
+    mouseOver: function (event) {
+        title.style.color = colors[0];
+        title.innerText = "The mouse is here!";
+    },
+    mouseLeave: function (event) {
+        title.style.color = colors[1];
+        title.innerText = "The mouse is gone!";
+    },
+    resized: function (event) {
+        title.style.color = colors[2];
+        title.innerText = "You just resized!";
+    },
+    click: function (event) {
+        title.style.color = colors[3];
+        title.innerText = "That was a right click!";
+    }
+};
+
+title.addEventListener("mouseover", superEventHandler.mouseOver);
+title.addEventListener("mouseleave", superEventHandler.mouseLeave);
+window.addEventListener("resize", superEventHandler.resized);
+title.addEventListener("click", superEventHandler.click);
